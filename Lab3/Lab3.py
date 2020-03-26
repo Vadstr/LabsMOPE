a271b673a06b2b03b9643fd731593b9751f0240c
vadim <Vadstr23@gmail.com>
2020-03-26 22:40:59 +0200

    edit lab3

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -3,15 +3,16 @@ import itertools
 import numpy as np
 import scipy
 from scipy.stats import f
-from scipy.stats import t
 from prettytable import PrettyTable
 
 
 def f_critical(prob, f1, f2):
     return scipy.stats.f.ppf(prob, f1, f2)
 
+
 def t_critical(prob, df):
     return scipy.stats.t.ppf(prob, df)
+
 
 def c_critical(prob, f1, f2):
     return 1 / (1 + (f2 - 1) / scipy.stats.f.ppf(1 - (1 - prob) / f2, f1, (f2 - 1) * f1))
@@ -31,15 +32,15 @@ xn = [combinations[8],
       combinations[11],
       combinations[13],
       combinations[14]]
-x = [[ min(x_bounds[j]) if xn[i][j] < 0 else max(x_bounds[j])
-        for j in range(len(x_bounds))]
-    for i in range(len(xn))]
+x = [[min(x_bounds[j]) if xn[i][j] < 0 else max(x_bounds[j])
+      for j in range(len(x_bounds))]
+     for i in range(len(xn))]
 
 y_bounds = [int(200 + np.mean([min(x_bounds[i]) for i in range(1, factors)])),
-    int(200 + np.mean([max(x_bounds[i]) for i in range(1, factors)])),]
+            int(200 + np.mean([max(x_bounds[i]) for i in range(1, factors)])), ]
 
 
-def create_matrix():
+def create_matrix(y):
     table = PrettyTable()
     table_head = ["Екперемент №"]
     for i in range(factors):
@@ -54,72 +55,87 @@ def create_matrix():
         table.add_row([i + 1, *xn[i], *y[i]])
     return table
 
-while True:
-    y = [
-        [random.randint(min(y_bounds), max(y_bounds)) for i in range(samples)]
-        for j in range(experiments)
-    ]
-    matrix = create_matrix()
 
-    s2_y = [np.var(y[i]) for i in range(experiments)]
-    stat_c = max(s2_y) / sum(s2_y)
-    crit_c = c_critical(confidence_prob, samples - 1, experiments)
+def cochrans_test(samples):
+    while True:
+        y = [
+            [random.randint(min(y_bounds), max(y_bounds)) for i in range(samples)]
+            for j in range(experiments)
+        ]
+        matrix = create_matrix(y)
 
-    print(matrix)
-    print(f"Розрахункова статистика С: {round(stat_c, 3)}")
-    print(
-        f"Критичний C для вірогідності довіри до {confidence_prob}: {round(crit_c, 3)}"
-    )
+        s2_y = [np.var(y[i]) for i in range(experiments)]
+        stat_c = max(s2_y) / sum(s2_y)
+        crit_c = c_critical(confidence_prob, samples - 1, experiments)
 
-    if stat_c < crit_c:
-        print("Варіанти рівні.")
-        break
+        print(matrix)
+        print(f"Розрахункова статистика С: {round(stat_c, 3)}")
+        print(
+            f"Критичний C для вірогідності довіри до {confidence_prob}: {round(crit_c, 3)}"
+        )
 
-    print("Варіанти не рівні. Збільшення розміру вибірки ...")
-    samples += 1
+        if stat_c < crit_c:
+            print("Варіанти рівні.")
+            break
 
-my = [np.mean(y[i]) for i in range(len(y))]
+        print("Варіанти не рівні. Збільшення розміру вибірки ...")
+        samples += 1
 
-xn_col = np.array(list(zip(*xn)))
-beta = [np.mean(my * xn_col[i]) for i in range(experiments)]
+    my = [np.mean(y[i]) for i in range(len(y))]
 
-yn = [sum(beta * np.array(xn[i])) for i in range(experiments)]
+    xn_col = np.array(list(zip(*xn)))
+    beta = [np.mean(my * xn_col[i]) for i in range(experiments)]
 
-print(f"Середні: {[round(my[i], 3) for i in range(experiments)]}")
-print(f"Обчислена функції: {[round(yn[i], 3) for i in range(experiments)]}")
+    yn = [sum(beta * np.array(xn[i])) for i in range(experiments)]
 
-delta_x = [abs(x_bounds[i][0] - x_bounds[i][1]) / 2 for i in range(len(x_bounds))]
-x0 = [(x_bounds[i][0] + x_bounds[i][1]) / 2 for i in range(len(x_bounds))]
-b = [beta[0] - sum(beta[i] * x0[i] / delta_x[i] for i in range(1, factors))]
-b.extend([beta[i] / delta_x[i] for i in range(1, factors)])
+    print(f"Середні: {[round(my[i], 3) for i in range(experiments)]}")
+    print(f"Обчислена функції: {[round(yn[i], 3) for i in range(experiments)]}")
 
-s2_b = np.mean(s2_y)
-s_beta = np.sqrt(s2_b / samples / experiments)
-stat_t = [abs(beta[i]) / s_beta for i in range(factors)]
+    delta_x = [abs(x_bounds[i][0] - x_bounds[i][1]) / 2 for i in range(len(x_bounds))]
+    x0 = [(x_bounds[i][0] + x_bounds[i][1]) / 2 for i in range(len(x_bounds))]
+    b = [beta[0] - sum(beta[i] * x0[i] / delta_x[i] for i in range(1, factors))]
+    b.extend([beta[i] / delta_x[i] for i in range(1, factors)])
+    return s2_y, b, my, beta
 
-crit_t = t_critical(confidence_prob, (samples - 1) * experiments)
 
-print(f"Обчислена t статистика: {[round(stat_t[i], 3) for i in range(len(stat_t))]}")
-print(f"Критичний t для вірогідності довіри {confidence_prob}: {round(crit_t, 3)}")
+def student_test(s2_y, beta, b):
+    s2_b = np.mean(s2_y)
+    s_beta = np.sqrt(s2_b / samples / experiments)
+    stat_t = [abs(beta[i]) / s_beta for i in range(factors)]
 
-significant_coeffs = 0
-for i in range(len(stat_t)):
-    if stat_t[i] < crit_t:
-        b[i] = 0
-        significant_coeffs += 1
+    crit_t = t_critical(confidence_prob, (samples - 1) * experiments)
 
-print(f"Коефіцієнти регресії: {[round(b[i], 3) for i in range(len(b))]}")
+    print(f"Обчислена t статистика: {[round(stat_t[i], 3) for i in range(len(stat_t))]}")
+    print(f"Критичний t для вірогідності довіри {confidence_prob}: {round(crit_t, 3)}")
 
-y_calc = [sum((b * np.array(x))[i]) for i in range(experiments)]
-print(f"Розрахункові значення моделі: {[round(y_calc[i], 3) for i in range(len(y_calc))]}")
+    significant_coeffs = 0
+    for i in range(len(stat_t)):
+        if stat_t[i] < crit_t:
+            b[i] = 0
+            significant_coeffs += 1
 
-s2_adeq = ( samples / (experiments - significant_coeffs) * sum([(y_calc[i] - my[i]) ** 2 for i in range(experiments)]))
+    print(f"Коефіцієнти регресії: {[round(b[i], 3) for i in range(len(b))]}")
 
-stat_f = s2_adeq / s2_b
-crit_f = f_critical(confidence_prob, (samples - 1) * experiments, experiments - significant_coeffs)
+    y_calc = [sum((b * np.array(x))[i]) for i in range(experiments)]
+    print(f"Розрахункові значення моделі: {[round(y_calc[i], 3) for i in range(len(y_calc))]}")
+    return significant_coeffs, s2_b, y_calc
 
-print(f"Розрахункова статистика F: {round(stat_f, 3)}")
-print(f"Критичний F для вірогідності довіри до {confidence_prob}: {round(crit_f, 3)}")
+
+def fisher_test(s2_b, significant_coeffs, y_calc, my):
+    s2_adeq = (samples / (experiments - significant_coeffs) * sum(
+        [(y_calc[i] - my[i]) ** 2 for i in range(experiments)]))
+
+    stat_f = s2_adeq / s2_b
+    crit_f = f_critical(confidence_prob, (samples - 1) * experiments, experiments - significant_coeffs)
+
+    print(f"Розрахункова статистика F: {round(stat_f, 3)}")
+    print(f"Критичний F для вірогідності довіри до {confidence_prob}: {round(crit_f, 3)}")
+    return stat_f, crit_f
+
+
+s2_y, b, my, beta = cochrans_test(samples)
+significant_coeffs, s2_b, y_calc = student_test(s2_y, beta, b)
+stat_f, crit_f = fisher_test(s2_b, significant_coeffs, y_calc, my)
 
 if stat_f > crit_f:
     print("Модель неадекватна.")
